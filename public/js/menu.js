bc9a0848d112496b98f38c76830193ae0bbe5e36
Sharon-Vovsi <164954218+Sharon-Vovsi@users.noreply.github.com>
2025-12-06 16:49:27 -0500

    Update menu.js

--- public/js/menu.js
+++ public/js/menu.js
@@ -3,17 +3,6 @@ function playGame() {
     const url = '/cardgame';
     window.open(url, "_self");
 }
-
-/*open login*/
-function openLogin() {
-    const url = '/login';
-    window.open(url, "_self");
-}
-
-/*open register*/
-function register() {
-    const url = '/register';
-    window.open(url, "_self");
-}
 /*--------------------------------------------------*/
+
 
