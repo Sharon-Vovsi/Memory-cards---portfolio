196889c5e1c7577bb7eba24c51b715d82f86f472
Sharon-Vovsi <164954218+Sharon-Vovsi@users.noreply.github.com>
2025-12-05 20:13:26 -0500

    Add files via upload

---
+++ public/js/menu.js
@@ -0,0 +1,19 @@
+/*open the board*/
+function playGame() {
+    const url = '/cardgame';
+    window.open(url, "_self");
+}
+
+/*open login*/
+function openLogin() {
+    const url = '/login';
+    window.open(url, "_self");
+}
+
+/*open register*/
+function register() {
+    const url = '/register';
+    window.open(url, "_self");
+}
+/*--------------------------------------------------*/
+
